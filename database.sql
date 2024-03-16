ed330367fb42928559c5eb5877baafea51ece94f
yung tech <76264891+Steeve-Crypto@users.noreply.github.com>
2024-03-16 10:49:28 -0400

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,60 +1,59 @@
-CREATE TABLE `Chefs Database` (
-  `Chief Id` int,
-  `Name` str,
-  `Password` char,
-  `Street` Str,
-  `Zipcode` int,
-  `number` int,
-  `Menu` str
+CREATE TABLE 'Chefs Database' (
+  'Chief Id' int,
+  'Name' char,
+  'Password' char,
+  'Street' char,
+  'Zipcode' int,
+  'number' int,
+  'Menu' char
 );
 
-CREATE TABLE `Dietary restrictions` (
-  `Field` <type>,
-  `Field` <type>
+CREATE TABLE 'Dietary restrictions' (
+  'item 1' char,
 );
 
-CREATE TABLE `Favorites dishes` (
-  `Field` Type,
-  `Field` Type,
-  `Field` Type
+CREATE TABLE 'Favorites dishes' (
+  'dish 1' char,
+  'dish 2' char,
 );
 
-CREATE TABLE `Menu items` (
-  `Field` <type>,
-  `Field` <type>,
-  `Field` <type>
+CREATE TABLE 'Menu items' (
+  'item 1' char ,
+  'item 2' char,
+  'item 3' char
 );
 
-CREATE TABLE `Consumers database` (
-  `name` str,
-  `password` char,
-  `Favorites dish` char,
-  `Dietary restrictions` char
+CREATE TABLE 'Consumers database' (
+  'name' char,
+  'password' char,
+  'Favorites dish' char,
+  'Dietary restrictions' char
 );
 
-CREATE TABLE `Payment method` (
-  `Id` <type>,
-  `Card number` <type>,
-  `Expiration  date` <type>
+CREATE TABLE 'Payment' (
+  'Id' int,
+  'Card number' int,
+  'card name' char,
+  'Expiration  date' char,
 );
 
-CREATE TABLE `Order` (
-  `order id` <type>,
-  `Amount` <type>,
-  `Date` <type>
+CREATE TABLE 'Order' (
+  'order id' int,
+  'Amount' int,
+  'Date' char
 );
 
-CREATE TABLE `Payment method` (
-  `Id` <type>,
-  `Card number` <type>,
-  `Expiration  date` <type>,
-  `Field` <type>
+CREATE TABLE 'Payment method' (
+  'Id' int,
+  'Card number' int,
+  'Card name' char,
+  'Expiration date' char,
 );
 
-CREATE TABLE `Subscription receipt` (
-  `order id` <type>,
-  `Amount` <type>,
-  `Date` <type>,
-  FOREIGN KEY (`Date`) REFERENCES `Payment method`(`Field`)
+CREATE TABLE 'Subscription receipt' (
+  'order id' int,
+  'Amount' int,
+  'Date' char,
+  FOREIGN KEY ('Date') REFERENCES 'Payment method'('Field')
 );
 
